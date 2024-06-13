6b6cac328d37c190621ab06ad7b486b5bf4e3a8f
adson <ficshonn@gmail.com>
2024-06-13 11:07:33 -0300

    Validação campos do formulário

--- js/form.js
+++ js/form.js
@@ -4,18 +4,24 @@ botaoAdicionar.addEventListener("click", function (event) {
     event.preventDefault();
 
     var form = document.querySelector("#form-adiciona");
+
     //Extraindo informações do paciente do form
     var paciente = obtemPacienteDoFormulario(form);
 
-    //Cria a tr e a td do paciente
-    var pacienteTr = montaTr(paciente);
+    var erro = validaPaciente(paciente);
 
+    if (erro.length > 0) {
+        exibeMensagemDeErro(erro);
+
+        return;
+    }
+    
+    adicionaPacienteNaTabela(paciente);
     form.reset();
-
-    var tabela = document.querySelector("#tabela-pacientes");
-
-    tabela.appendChild(pacienteTr);
-
+    
+    var msgErro = document.querySelector("#msg-erro")
+    msgErro.innerHTML = "";
+    
 })
 
 function obtemPacienteDoFormulario(form) {
@@ -28,6 +34,10 @@ function obtemPacienteDoFormulario(form) {
     return paciente;
 }
 
+
+
+
+    
 
 //cria a tr e a td da lista
 function montaTr(paciente) {
@@ -49,10 +59,65 @@ function montaTr(paciente) {
     return pacienteTr;
 }
 
+
+
 function montaTd(dado, classe) {
     var td = document.createElement("td");
     td.textContent = dado;
     td.classList.add(classe);
 
     return td;
+}
+
+
+function validaPaciente(paciente) {
+    var erro = [];
+
+    if (paciente.nome == "" || paciente.nome == null) {
+        erro.push("Nome não pode ser vazio!");
+    }
+    if (paciente.peso == "" || paciente.peso == null){
+        erro.push("Peso não pode ser vazio!");
+    }
+    if (paciente.altura == "" || paciente.altura == null){
+        erro.push ("Altura não pode ser vazio!");
+    }
+    if (paciente.gordura == "" || paciente.gordura == null){
+        erro.push("Gordura não pode ser vazio!");
+    }
+
+    return erro;
+}
+
+function validaPeso(peso) {
+    if (peso > 0 && peso <=600) {
+        return true;
+    }
+    return false;
+}
+
+function validaAltura(altura) {
+    if (altura > 0 && altura <= 3.00) {
+        return true;
+    }
+    return false;
+}
+
+function exibeMensagemDeErro(erro) {
+    var msgErro = document.querySelector("#msg-erro")
+    msgErro.innerHTML = "";
+    erro.forEach(erro => {
+        var li = document.createElement("li");
+        li.textContent = erro;
+        msgErro.appendChild(li);
+
+    });
+
+    return;
+}
+
+function adicionaPacienteNaTabela(paciente){
+    var pacienteTr = montaTr(paciente);
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
 }
